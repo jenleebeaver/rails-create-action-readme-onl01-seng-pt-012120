87dc7c0e9584271f7720d8869ac6babc09dfbf65
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-27 23:29:04 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,8 +14,8 @@ class PostsController < ApplicationController
   # add create method here
   def create
     post = Post.new
-post.title = params[:title]
-post.description = params[:description]
-post.save
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
   end
 end
